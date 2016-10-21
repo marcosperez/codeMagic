3a69e3b887f435c32a349e0e057bc8b03034ead8
Marcos perez <marcos.d.perez@gmail.com>
2016-10-21 13:10:44 -0300

    rooms

--- cliente/app/servicios/sSocket.js
+++ cliente/app/servicios/sSocket.js
@@ -1,49 +1,45 @@
-app.service('sSocket', ['$log','$rootScope','$q', function ($log,$rootScope,$q) {
-    var sSocket =  this;
+app.service('sSocket', ['$log', '$rootScope', '$q', function ($log, $rootScope, $q) {
+    var sSocket = this;
     sSocket.socket = false;
-    sSocket.obtenerSocket = function(){
-
+    sSocket.obtenerSocket = function () {
         var deferred = $q.defer();
-
-
-        if(sSocket.socket){
+        if (sSocket.socket) {
             deferred.resolve(sSocket.socket);
-        }else{
+        }
+        else {
             sSocket.socket = io('http://172.16.22.114:8001');
-
-            //informo de mi presencia
-            console.log($rootScope.usuario);
-            sSocket.socket.emit('usuario conectado',$rootScope.usuario);
-
-            //Recibo el resto de los usuarios
-            sSocket.socket.on('usuarios',function(data){
-                $rootScope.usuarios = data;
-                $rootScope.$broadcast('usuarios', data);
-            });
-
-            //Agrego un usuario nuevo
-            sSocket.socket.on('usuario conectado',function(data){
-                $rootScope.usuarios.push(data);
-                $rootScope.$broadcast('usuarios', data);
-            });
-
-            //Quito el usuario desconectado
-            sSocket.socket.on('usuario desconectado',function(data){
-                $rootScope.usuarios.splice($rootScope.usuarios.indexOf(data),1);
-                $rootScope.$broadcast('usuarios', data);
-            });
-
-            sSocket.socket.on('connect',function(socket){
+            //Conectados
+            sSocket.socket.on('connect', function (socket) {
                 deferred.resolve(sSocket.socket);
+                sSocket.cargarProyecto = function (abrirProyecto) {
+                    sSocket.socket.emit('setProyecto', {
+                        idProyecto: idProyecto
+                    });
+                    //informo de mi presencia
+                    console.log($rootScope.usuario);
+                    sSocket.socket.emit('usuario conectado', $rootScope.usuario);
+                    //Recibo el resto de los usuarios
+                    sSocket.socket.on('usuarios', function (data) {
+                        $rootScope.usuarios = data;
+                        $rootScope.$broadcast('usuarios', data);
+                    });
+                    //Agrego un usuario nuevo
+                    sSocket.socket.on('usuario conectado', function (data) {
+                        $rootScope.usuarios.push(data);
+                        $rootScope.$broadcast('usuarios', data);
+                    });
+                    //Quito el usuario desconectado
+                    sSocket.socket.on('usuario desconectado', function (data) {
+                        $rootScope.usuarios.splice($rootScope.usuarios.indexOf(data), 1);
+                        $rootScope.$broadcast('usuarios', data);
+                    });
+                }
             });
-
-            sSocket.socket.on('error',function(err){
+            //Error
+            sSocket.socket.on('error', function (err) {
                 deferred.reject(err);
             });
-
         }
         return deferred.promise;
-
     }
-
 }])
--- server/utiles/socket.js
+++ server/utiles/socket.js
@@ -5,6 +5,38 @@ const fs = require('fs');
 var usuarios = [];
 
 io.on('connection', function (socket) {
+        //Archivos y proyectos---------------------------------------------------------------
+    socket.on('abrirProyecto',function(data){
+        console.log("usuario join: "+data.idProyecto);
+        socket.idProyecto = data.idProyecto;
+        socket.join(data.idProyecto);
+    });
+
+    socket.on('guardar', function (data) {
+        var nombreArchivo = data.nombreArchivo ? data.nombreArchivo : 'archivo.js';
+        fs.writeFile(nombreArchivo, data.archivo, 'utf8', function (err) {
+            if (err) return console.log(err);
+            console.log('Se guardo correctamente');
+        });
+    });
+
+    socket.on('recuperar proyectos',function(){
+
+    });
+
+    socket.on('recuperar archivo', function (data) {
+        var nombreArchivo = data.nombreArchivo ? data.nombreArchivo : 'archivo.js';
+        fs.readFile(nombreArchivo, 'utf8', (err, archivo) => {
+            if (err) //throw err;
+                console.log("El archivo no existe");
+            console.log(archivo);
+            socket.to(socket.idProyecto).("archivo", {
+                archivo: archivo
+            });
+        });
+    });
+    //--------------------------------------------------------------------------
+
     //Usuarios-----------------------------------------------
     socket.on('usuario conectado', function (data) {
         console.log("Se conecto "+data.nombre);
@@ -33,31 +65,4 @@ io.on('connection', function (socket) {
         socket.emit('accion', data);
     });
     //-------------------------------------------------------------------
-
-
-    //Archivos y proyectos---------------------------------------------------------------
-    socket.on('guardar', function (data) {
-        var nombreArchivo = data.nombreArchivo ? data.nombreArchivo : 'archivo.js';
-        fs.writeFile(nombreArchivo, data.archivo, 'utf8', function (err) {
-            if (err) return console.log(err);
-            console.log('Se guardo correctamente');
-        });
-    });
-
-    socket.on('recuperar proyectos',function(){
-
-    });
-
-    socket.on('recuperar archivo', function (data) {
-        var nombreArchivo = data.nombreArchivo ? data.nombreArchivo : 'archivo.js';
-        fs.readFile(nombreArchivo, 'utf8', (err, archivo) => {
-            if (err) //throw err;
-                console.log("El archivo no existe");
-            console.log(archivo);
-            socket.emit("archivo", {
-                archivo: archivo
-            });
-        });
-    });
-    //----------------------------------------------------------------------------------------
 });
